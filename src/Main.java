5b3298d03434f7427adac1398595b2df251ac9d3
ValerianSid <valsid.sidorov@yandex.ru>
2022-07-14 14:24:47 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
 public class Main {
 
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+        queue.add("sadas");
+        System.out.println(queue.peek());
+        System.out.println(queue);
+    }
 }
--- src/Queue.java
+++ src/Queue.java
@@ -1,13 +1,56 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class Queue {
 
     private List<String> list = new ArrayList<>();
+    private int size = list.size();
 
-    public Queue(List<String> list) {
+    public Queue() {
         this.list = list;
     }
 
+    public boolean add(String str){
+        if (0 < 0){
+            return false;
+        }
+        list.add(0, str);
+        return true;
+    }
+    public String element(){
+        if(list.isEmpty()){
+            throw new NoSuchElementException();
+        }
+        return list.get(list.size() - 1);
+    }
+    public void offer(){
 
+    }
+    public String peek(){
+        return list.isEmpty() ? null : list.get(list.size() - 1);
+    }
+    public String poll(){
+        if(list.isEmpty()){
+            return null;
+        }
+        String str = list.get(list.size() - 1);
+        list.remove(list.size() - 1);
+        return str;
+    }
+    public String remove(){
+        if(list.isEmpty()){
+            throw new NoSuchElementException();
+        }
+        String str = list.get(list.size() - 1);
+        list.remove(list.size() - 1);
+        return str;
+    }
+
+    @Override
+    public String toString() {
+        return "Queue{" +
+                "list=" + list +
+                '}';
+    }
 }

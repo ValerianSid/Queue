b50f4f1fc645f592225bce600bba429c483739cc
ValerianSid <valsid.sidorov@yandex.ru>
2022-07-14 13:16:33 +0300

    first try

---
+++ src/Queue.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Queue {
+
+    private List<String> list = new ArrayList<>();
+
+    public Queue(List<String> list) {
+        this.list = list;
+    }
+
+
+}
